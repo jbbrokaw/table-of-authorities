10fdfbf4db731d6182399131e46838140b3be014
jbbrokaw <jbrokaw@gmail.com>
2015-01-28 22:41:53 -0800

    Adds function to find cites without style info (just one for now)

--- parser.py
+++ parser.py
@@ -2,6 +2,13 @@
 from __future__ import unicode_literals
 import re
 
+ALLOWED_SYMBOLS = {'&'}
+ABBREVIATIONS = {'Co.',
+                 'U.S.',
+                 'Corp.',
+                 'Inc.',
+                 'Dist.'}
+
 
 def find_full_cites(doc):
     date_re = re.compile('\d{4}\)')
@@ -23,3 +30,24 @@ def find_full_cites(doc):
                 )
                 if date_re.search(cite_string):
                     print cite_string
+
+
+def find_cites_by_word(text):
+    words = text.split()
+    date_re = re.compile('\d{4}\)')
+    if 'v.' not in words:
+        return
+
+    v_index = words.index('v.')
+    begin_index = v_index - 1
+    while (words[begin_index][-1] != '.') or \
+            (words[begin_index] in ABBREVIATIONS):
+        begin_index += 1
+    begin_index = min(v_index - 1, begin_index + 1)
+    end_index = v_index + 1
+    while (end_index < len(words)) and \
+            (date_re.search(words[end_index]) is None):
+        end_index += 1
+    cite_string = " ".join(words[begin_index:end_index + 1])
+    if date_re.search(cite_string):
+        print cite_string
